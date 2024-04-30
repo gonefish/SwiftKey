362314d3596b524d5deb79be559c3ad4bb48d132
Q.GuoQiang <gonefish@gmail.com>
2024-04-30 23:27:37 +0800

    Update

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,9 +7,9 @@ let package = Package(
     name: "SwiftKey",
     platforms: [
         .macOS(.v10_13),
-        .iOS(.v11),
+        .iOS(.v12),
         .watchOS(.v4),
-        .tvOS(.v11)
+        .tvOS(.v12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
