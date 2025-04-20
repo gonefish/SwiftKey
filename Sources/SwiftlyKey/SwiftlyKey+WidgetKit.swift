e20333c379a590fef32e86fb06ed2537ff8e385e
Q.GuoQiang <gonefish@gmail.com>
2025-04-20 22:08:39 +0800

    Mark public for extension

--- Sources/SwiftlyKey/SwiftlyKey+WidgetKit.swift
+++ Sources/SwiftlyKey/SwiftlyKey+WidgetKit.swift
@@ -4,7 +4,7 @@ import WidgetKit
 import SwiftUI
 
 @available(macOS 11.0, iOS 14, watchOS 9, *)
-extension StaticConfiguration {
+public extension StaticConfiguration {
 
     @MainActor @preconcurrency init<Provider>(kindKey: any RawRepresentable<SwiftlyKey>, provider: Provider, @ViewBuilder content: @escaping (Provider.Entry) -> Content) where Provider : TimelineProvider {
         self.init(kind: kindKey.name, provider: provider, content: content)
